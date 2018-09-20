7d6035685dafa528bf918609fdbf69bfb8f57d07
Castracani <willmyers993@gmail.com>
2018-09-20 16:56:39 -0400

    creating basic structure for vital files; unable to resolve react-router-dom error

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,4 +1,21 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Game from "./pages/Game";
+import Wrapper from "./components/Wrapper";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
+const App = () => (
+  <Router>
+    <div>
+      <Navbar />
+      <Wrapper>
+        <Route exact path="/" component={Game} />
+        
+      </Wrapper>
+      <Footer />
+    </div>
+  </Router>
+);
 
+export default App;
---
+++ memory-game/src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <span>Memory Game 2018 by W.M.</span>
+  </footer>
+);
+
+export default Footer;
